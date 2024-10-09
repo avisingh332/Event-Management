df7c5eef7e5ce12b00bd9400646428becd3ff488
Avinash Singh <avisingh332@gmail.com>
2024-10-09 14:37:48 +0530

    First commit

---
+++ Event Management/Event Management.Data/Models/Event.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_Management.Data.Models
+{
+    public class Event
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public string Location { get; set; }
+        public DateTime DateTime { get; set; }
+        public DateTime CreatedAt { get; set; }
+        [ForeignKey("Organizer")]
+        public Guid OrganizerId { get; set; }
+        
+        //navigation Property
+        public ApplicationUser Organizer { get; set; }
+        public ICollection<ApplicationUser> Attendees { get; set; }
+
+    }
+}
